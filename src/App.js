e23aa7884ed0d221eda89f114a635b2e65a32477
ohyaelim <npr05080@gmail.com>
2021-01-19 23:24:15 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
-}
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <Food  fav="kimchi"  />
-      <Food  fav="ramen"  />
-      <Food  fav="samgiopsal"  />
-      <Food  fav="pizza"  />
-    </div>
-  );
+class App extends React.Component{
+  state={
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+   render(){
+     return (
+       <div>
+         <h1>The number is: {this.state.count}</h1>
+         <button onClick={this.add}>Add</button>
+         <button onClick={this.minus}>Minus</button>
+       </div>
+     );
+   }
 }
 
 export default App;
