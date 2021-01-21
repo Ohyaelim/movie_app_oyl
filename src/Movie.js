df92bbb5d4eed13538d12530fcf9de299be360d3
ohyaelim <npr05080@gmail.com>
2021-01-21 22:50:51 +0900

    #4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -5,15 +5,15 @@ import "./Movie.css"
 function Movie({ id, year, title, summary, poster ,genres}) {
   return <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h4 className="movie_year">{year}</h4>
-        <ul className="genres">{genres.map( (genre, index) => (
+      <div className="movie__ data">
+        <h3 className="movie__title">{title}</h3>
+        <h4 className="movie__year">{year}</h4>
+        <ul className="movie__genres">{genres.map( (genre, index) => (
             <li key={index} className="genres__genre">
                 {genre}
             </li>))}
         </ul>
-        <p className="movie_summary">{summary}</p>
+        <p className="movie_summary">{summary.slice(0,140)}...</p>
       </div>
   </div>;
 }
